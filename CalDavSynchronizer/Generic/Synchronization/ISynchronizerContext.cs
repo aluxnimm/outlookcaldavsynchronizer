389074b6fdaef1d3e70451c7038367bf3cfcadbf
Gerhard Zehetbauer <se13m017@technikum-wien.at>
2015-03-23 01:01:01 +0100

    updated license headers

--- CalDavSynchronizer/Generic/Synchronization/ISynchronizerContext.cs
+++ CalDavSynchronizer/Generic/Synchronization/ISynchronizerContext.cs
@@ -13,7 +13,6 @@
 // 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System;
 using CalDavSynchronizer.EntityRepositories;
 using CalDavSynchronizer.Generic.EntityMapping;
--- CalDavSynchronizer/Implementation/InitialSyncStateCreationStrategyFactory.cs
+++ CalDavSynchronizer/Implementation/InitialSyncStateCreationStrategyFactory.cs
@@ -13,7 +13,6 @@
 // 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System;
 using CalDavSynchronizer.Generic.EntityRelationManagement;
 using CalDavSynchronizer.Generic.Synchronization;
