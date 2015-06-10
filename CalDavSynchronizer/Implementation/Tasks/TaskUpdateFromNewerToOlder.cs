5a819f7ec768c95359cb18c8d7e5efd65bee8ab1
Gerhard Zehetbauer <se13m017@technikum-wien.at>
2015-06-10 19:02:05 +0200

    Add components for task synchronization

---
+++ CalDavSynchronizer/Implementation/Tasks/TaskUpdateFromNewerToOlder.cs
@@ -0,0 +1,46 @@
+// This file is Part of CalDavSynchronizer (http://outlookcaldavsynchronizer.sourceforge.net/)
+// Copyright (c) 2015 Gerhard Zehetbauer 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using CalDavSynchronizer.Generic.EntityRelationManagement;
+using CalDavSynchronizer.Generic.Synchronization;
+using CalDavSynchronizer.Generic.Synchronization.States;
+using CalDavSynchronizer.Implementation.ComWrappers;
+using DDay.iCal;
+
+namespace CalDavSynchronizer.Implementation.Tasks
+{
+  internal class TaskUpdateFromNewerToOlder
+      : UpdateFromNewerToOlder<string, DateTime, TaskItemWrapper, Uri, string, ITodo>
+  {
+    public TaskUpdateFromNewerToOlder (EntitySyncStateEnvironment<string, DateTime, TaskItemWrapper, Uri, string, ITodo> environment, IEntityRelationData<string, DateTime, Uri, string> knownData, DateTime newA, string newB)
+        : base (environment, knownData, newA, newB)
+    {
+    }
+
+    protected override bool AIsNewerThanB
+    {
+      get
+      {
+        // Assume that no modification means, that the item is never modified. Therefore it must be new. 
+        if (_bEntity.LastModified == null)
+          return false;
+
+        return _aEntity.Inner.LastModificationTime.ToUniversalTime () >= _bEntity.LastModified.UTC;
+      }
+    }
+  }
+}
---
+++ CalDavSynchronizer/Implementation/Tasks/TasktEntityConflictSyncStateFactory_Automatic.cs
@@ -0,0 +1,46 @@
+// This file is Part of CalDavSynchronizer (http://outlookcaldavsynchronizer.sourceforge.net/)
+// Copyright (c) 2015 Gerhard Zehetbauer 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using CalDavSynchronizer.Generic.EntityRelationManagement;
+using CalDavSynchronizer.Generic.Synchronization;
+using CalDavSynchronizer.Generic.Synchronization.StateFactories;
+using CalDavSynchronizer.Generic.Synchronization.States;
+using CalDavSynchronizer.Implementation.ComWrappers;
+using CalDavSynchronizer.Implementation.Events;
+using DDay.iCal;
+
+namespace CalDavSynchronizer.Implementation.Tasks
+{
+  internal class TaskEntityConflictSyncStateFactory_Automatic
+      : EntityConflictSyncStateFactory_Automatic<string, DateTime, TaskItemWrapper, Uri, string, ITodo>
+  {
+    public TaskEntityConflictSyncStateFactory_Automatic (EntitySyncStateEnvironment<string, DateTime, TaskItemWrapper, Uri, string, ITodo> environment)
+        : base (environment)
+    {
+    }
+
+    protected override IEntitySyncState<string, DateTime, TaskItemWrapper, Uri, string, ITodo> Create_FromNewerToOlder (IEntityRelationData<string, DateTime, Uri, string> knownData, DateTime newA, string newB)
+    {
+      return new TaskUpdateFromNewerToOlder (
+          _environment,
+          knownData,
+          newA,
+          newB
+          );
+    }
+  }
+}
