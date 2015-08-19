1dfa5513a2fb73db93d6e94ba5d068077ca32ccc
Gerhard Zehetbauer <se13m017@technikum-wien.at>
2015-08-19 22:38:35 +0200

    Add AssemblyInfoCommon

--- GenSync/Properties/AssemblyInfo.cs
+++ GenSync/Properties/AssemblyInfo.cs
@@ -6,13 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle ("GenSync")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("GenSync")]
-[assembly: AssemblyCopyright ("Copyright ©  2015")]
-[assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -36,5 +29,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
