babf33e08491242af04ec7739d77138bfde279df
Alexander Nimmervoll <nimm@technikum-wien.at>
2015-11-14 19:45:28 +0100

    Resize EventMapping Form to show buttons when bigger Windows Font is used

--- CalDavSynchronizer/Ui/EventMappingConfigurationForm.Designer.cs
+++ CalDavSynchronizer/Ui/EventMappingConfigurationForm.Designer.cs
@@ -39,9 +39,10 @@
       // 
       this._cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this._cancelButton.Location = new System.Drawing.Point(189, 111);
+      this._cancelButton.Location = new System.Drawing.Point(252, 144);
+      this._cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._cancelButton.Name = "_cancelButton";
-      this._cancelButton.Size = new System.Drawing.Size(75, 23);
+      this._cancelButton.Size = new System.Drawing.Size(100, 28);
       this._cancelButton.TabIndex = 0;
       this._cancelButton.Text = "Cancel";
       this._cancelButton.UseVisualStyleBackColor = true;
@@ -49,9 +50,10 @@
       // _okButton
       // 
       this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this._okButton.Location = new System.Drawing.Point(108, 111);
+      this._okButton.Location = new System.Drawing.Point(144, 144);
+      this._okButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._okButton.Name = "_okButton";
-      this._okButton.Size = new System.Drawing.Size(75, 23);
+      this._okButton.Size = new System.Drawing.Size(100, 28);
       this._okButton.TabIndex = 1;
       this._okButton.Text = "OK";
       this._okButton.UseVisualStyleBackColor = true;
@@ -60,9 +62,10 @@
       // _mapReminderCheckBox
       // 
       this._mapReminderCheckBox.AutoSize = true;
-      this._mapReminderCheckBox.Location = new System.Drawing.Point(12, 12);
+      this._mapReminderCheckBox.Location = new System.Drawing.Point(16, 15);
+      this._mapReminderCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._mapReminderCheckBox.Name = "_mapReminderCheckBox";
-      this._mapReminderCheckBox.Size = new System.Drawing.Size(95, 17);
+      this._mapReminderCheckBox.Size = new System.Drawing.Size(122, 21);
       this._mapReminderCheckBox.TabIndex = 2;
       this._mapReminderCheckBox.Text = "Map Reminder";
       this._mapReminderCheckBox.UseVisualStyleBackColor = true;
@@ -70,9 +73,10 @@
       // _mapAttendeesCheckBox
       // 
       this._mapAttendeesCheckBox.AutoSize = true;
-      this._mapAttendeesCheckBox.Location = new System.Drawing.Point(12, 35);
+      this._mapAttendeesCheckBox.Location = new System.Drawing.Point(16, 43);
+      this._mapAttendeesCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._mapAttendeesCheckBox.Name = "_mapAttendeesCheckBox";
-      this._mapAttendeesCheckBox.Size = new System.Drawing.Size(98, 17);
+      this._mapAttendeesCheckBox.Size = new System.Drawing.Size(125, 21);
       this._mapAttendeesCheckBox.TabIndex = 3;
       this._mapAttendeesCheckBox.Text = "Map Attendees";
       this._mapAttendeesCheckBox.UseVisualStyleBackColor = true;
@@ -80,9 +84,10 @@
       // _mapBodyCheckBox
       // 
       this._mapBodyCheckBox.AutoSize = true;
-      this._mapBodyCheckBox.Location = new System.Drawing.Point(12, 58);
+      this._mapBodyCheckBox.Location = new System.Drawing.Point(16, 71);
+      this._mapBodyCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._mapBodyCheckBox.Name = "_mapBodyCheckBox";
-      this._mapBodyCheckBox.Size = new System.Drawing.Size(74, 17);
+      this._mapBodyCheckBox.Size = new System.Drawing.Size(93, 21);
       this._mapBodyCheckBox.TabIndex = 4;
       this._mapBodyCheckBox.Text = "Map Body";
       this._mapBodyCheckBox.UseVisualStyleBackColor = true;
@@ -90,10 +95,10 @@
       // EventMappingConfigurationForm
       // 
       this.AcceptButton = this._okButton;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this._cancelButton;
-      this.ClientSize = new System.Drawing.Size(276, 146);
+      this.ClientSize = new System.Drawing.Size(368, 187);
       this.ControlBox = false;
       this.Controls.Add(this._mapBodyCheckBox);
       this.Controls.Add(this._mapAttendeesCheckBox);
@@ -101,6 +106,7 @@
       this.Controls.Add(this._okButton);
       this.Controls.Add(this._cancelButton);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.Name = "EventMappingConfigurationForm";
       this.ShowIcon = false;
       this.ShowInTaskbar = false;
