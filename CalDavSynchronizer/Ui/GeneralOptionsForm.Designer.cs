83148f1955a2bfc6524e53825711ac91065b0ff4
Alexander Nimmervoll <nimm@technikum-wien.at>
2015-11-15 22:39:08 +0100

    Resize GeneralOptionForm

--- CalDavSynchronizer/Ui/GeneralOptionsForm.Designer.cs
+++ CalDavSynchronizer/Ui/GeneralOptionsForm.Designer.cs
@@ -40,9 +40,10 @@
       // 
       this._cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this._cancelButton.Location = new System.Drawing.Point(189, 124);
+      this._cancelButton.Location = new System.Drawing.Point(252, 169);
+      this._cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._cancelButton.Name = "_cancelButton";
-      this._cancelButton.Size = new System.Drawing.Size(75, 23);
+      this._cancelButton.Size = new System.Drawing.Size(100, 28);
       this._cancelButton.TabIndex = 0;
       this._cancelButton.Text = "Cancel";
       this._cancelButton.UseVisualStyleBackColor = true;
@@ -50,9 +51,10 @@
       // _okButton
       // 
       this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this._okButton.Location = new System.Drawing.Point(108, 124);
+      this._okButton.Location = new System.Drawing.Point(144, 169);
+      this._okButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._okButton.Name = "_okButton";
-      this._okButton.Size = new System.Drawing.Size(75, 23);
+      this._okButton.Size = new System.Drawing.Size(100, 28);
       this._okButton.TabIndex = 1;
       this._okButton.Text = "OK";
       this._okButton.UseVisualStyleBackColor = true;
@@ -61,9 +63,10 @@
       // _checkForNewerVersionsCheckBox
       // 
       this._checkForNewerVersionsCheckBox.AutoSize = true;
-      this._checkForNewerVersionsCheckBox.Location = new System.Drawing.Point(12, 12);
+      this._checkForNewerVersionsCheckBox.Location = new System.Drawing.Point(16, 15);
+      this._checkForNewerVersionsCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._checkForNewerVersionsCheckBox.Name = "_checkForNewerVersionsCheckBox";
-      this._checkForNewerVersionsCheckBox.Size = new System.Drawing.Size(210, 17);
+      this._checkForNewerVersionsCheckBox.Size = new System.Drawing.Size(274, 21);
       this._checkForNewerVersionsCheckBox.TabIndex = 5;
       this._checkForNewerVersionsCheckBox.Text = "Automatically check for newer versions";
       this._checkForNewerVersionsCheckBox.UseVisualStyleBackColor = true;
@@ -71,9 +74,10 @@
       // _storeDataInRoamingFolderCheckBox
       // 
       this._storeDataInRoamingFolderCheckBox.AutoSize = true;
-      this._storeDataInRoamingFolderCheckBox.Location = new System.Drawing.Point(12, 35);
+      this._storeDataInRoamingFolderCheckBox.Location = new System.Drawing.Point(16, 43);
+      this._storeDataInRoamingFolderCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._storeDataInRoamingFolderCheckBox.Name = "_storeDataInRoamingFolderCheckBox";
-      this._storeDataInRoamingFolderCheckBox.Size = new System.Drawing.Size(155, 17);
+      this._storeDataInRoamingFolderCheckBox.Size = new System.Drawing.Size(206, 21);
       this._storeDataInRoamingFolderCheckBox.TabIndex = 6;
       this._storeDataInRoamingFolderCheckBox.Text = "Store data in roaming folder";
       this._toolTip.SetToolTip(this._storeDataInRoamingFolderCheckBox, "Changing this option requires a restart of Outlook.");
@@ -82,16 +86,17 @@
       // GeneralOptionsForm
       // 
       this.AcceptButton = this._okButton;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this._cancelButton;
-      this.ClientSize = new System.Drawing.Size(276, 158);
+      this.ClientSize = new System.Drawing.Size(368, 210);
       this.ControlBox = false;
       this.Controls.Add(this._storeDataInRoamingFolderCheckBox);
       this.Controls.Add(this._checkForNewerVersionsCheckBox);
       this.Controls.Add(this._okButton);
       this.Controls.Add(this._cancelButton);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.Name = "GeneralOptionsForm";
       this.ShowIcon = false;
       this.ShowInTaskbar = false;
