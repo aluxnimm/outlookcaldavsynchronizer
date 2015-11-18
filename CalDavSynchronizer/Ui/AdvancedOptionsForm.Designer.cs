0db92d45d29caa172b09b4ea60aa5773f1d45082
Alexander Nimmervoll <nimm@technikum-wien.at>
2015-11-18 15:25:27 +0100

    Layout changes for AdvancedOptionForm

--- CalDavSynchronizer/Ui/AdvancedOptionsForm.Designer.cs
+++ CalDavSynchronizer/Ui/AdvancedOptionsForm.Designer.cs
@@ -52,9 +52,10 @@
       // 
       this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.OkButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.OkButton.Location = new System.Drawing.Point(364, 362);
+      this.OkButton.Location = new System.Drawing.Point(485, 446);
+      this.OkButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.OkButton.Name = "OkButton";
-      this.OkButton.Size = new System.Drawing.Size(75, 23);
+      this.OkButton.Size = new System.Drawing.Size(100, 28);
       this.OkButton.TabIndex = 6;
       this.OkButton.Text = "OK";
       this.OkButton.UseVisualStyleBackColor = true;
@@ -64,9 +65,10 @@
       // 
       this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.buttonCancel.Location = new System.Drawing.Point(449, 362);
+      this.buttonCancel.Location = new System.Drawing.Point(599, 446);
+      this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.buttonCancel.Name = "buttonCancel";
-      this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+      this.buttonCancel.Size = new System.Drawing.Size(100, 28);
       this.buttonCancel.TabIndex = 7;
       this.buttonCancel.Text = "Cancel";
       this.buttonCancel.UseVisualStyleBackColor = true;
@@ -77,9 +79,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
       this.groupBox2.Controls.Add(this._useManualProxyCheckBox);
       this.groupBox2.Controls.Add(this._useSystemProxyCheckBox);
-      this.groupBox2.Location = new System.Drawing.Point(10, 107);
+      this.groupBox2.Location = new System.Drawing.Point(13, 132);
+      this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Size = new System.Drawing.Size(514, 204);
+      this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.groupBox2.Size = new System.Drawing.Size(685, 251);
       this.groupBox2.TabIndex = 8;
       this.groupBox2.TabStop = false;
       this.groupBox2.Text = "Proxy settings";
@@ -87,9 +91,10 @@
       // _useManualProxyCheckBox
       // 
       this._useManualProxyCheckBox.AutoSize = true;
-      this._useManualProxyCheckBox.Location = new System.Drawing.Point(13, 42);
+      this._useManualProxyCheckBox.Location = new System.Drawing.Point(17, 52);
+      this._useManualProxyCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._useManualProxyCheckBox.Name = "_useManualProxyCheckBox";
-      this._useManualProxyCheckBox.Size = new System.Drawing.Size(176, 17);
+      this._useManualProxyCheckBox.Size = new System.Drawing.Size(230, 21);
       this._useManualProxyCheckBox.TabIndex = 16;
       this._useManualProxyCheckBox.Text = "Use Manual Proxy configuration";
       this._useManualProxyCheckBox.UseVisualStyleBackColor = true;
@@ -98,9 +103,10 @@
       // _useSystemProxyCheckBox
       // 
       this._useSystemProxyCheckBox.AutoSize = true;
-      this._useSystemProxyCheckBox.Location = new System.Drawing.Point(13, 19);
+      this._useSystemProxyCheckBox.Location = new System.Drawing.Point(17, 23);
+      this._useSystemProxyCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._useSystemProxyCheckBox.Name = "_useSystemProxyCheckBox";
-      this._useSystemProxyCheckBox.Size = new System.Drawing.Size(298, 17);
+      this._useSystemProxyCheckBox.Size = new System.Drawing.Size(396, 21);
       this._useSystemProxyCheckBox.TabIndex = 15;
       this._useSystemProxyCheckBox.Text = "Use System Default Proxy (settings from IE and config file)";
       this._useSystemProxyCheckBox.UseVisualStyleBackColor = true;
@@ -109,53 +115,59 @@
       // label5
       // 
       this.label5.AutoSize = true;
-      this.label5.Location = new System.Drawing.Point(44, 77);
+      this.label5.Location = new System.Drawing.Point(6, 95);
+      this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(56, 13);
+      this.label5.Size = new System.Drawing.Size(137, 17);
       this.label5.TabIndex = 22;
-      this.label5.Text = "Password:";
+      this.label5.Text = "Password (optional):";
       // 
       // label4
       // 
       this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(44, 51);
+      this.label4.Location = new System.Drawing.Point(6, 63);
+      this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(58, 13);
+      this.label4.Size = new System.Drawing.Size(141, 17);
       this.label4.TabIndex = 21;
-      this.label4.Text = "Username:";
+      this.label4.Text = "Username (optional):";
       // 
       // _passwordTextBox
       // 
       this._passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this._passwordTextBox.Location = new System.Drawing.Point(307, 77);
+      this._passwordTextBox.Location = new System.Drawing.Point(409, 95);
+      this._passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._passwordTextBox.Name = "_passwordTextBox";
       this._passwordTextBox.PasswordChar = '*';
-      this._passwordTextBox.Size = new System.Drawing.Size(183, 20);
+      this._passwordTextBox.Size = new System.Drawing.Size(243, 22);
       this._passwordTextBox.TabIndex = 20;
       // 
       // _userNameTextBox
       // 
       this._userNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this._userNameTextBox.Location = new System.Drawing.Point(307, 51);
+      this._userNameTextBox.Location = new System.Drawing.Point(409, 63);
+      this._userNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._userNameTextBox.Name = "_userNameTextBox";
-      this._userNameTextBox.Size = new System.Drawing.Size(183, 20);
+      this._userNameTextBox.Size = new System.Drawing.Size(243, 22);
       this._userNameTextBox.TabIndex = 19;
       // 
       // _proxyUrlTextBox
       // 
       this._proxyUrlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this._proxyUrlTextBox.Location = new System.Drawing.Point(102, 25);
+      this._proxyUrlTextBox.Location = new System.Drawing.Point(155, 31);
+      this._proxyUrlTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._proxyUrlTextBox.Name = "_proxyUrlTextBox";
-      this._proxyUrlTextBox.Size = new System.Drawing.Size(388, 20);
+      this._proxyUrlTextBox.Size = new System.Drawing.Size(497, 22);
       this._proxyUrlTextBox.TabIndex = 17;
       // 
       // label3
       // 
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(44, 25);
+      this.label3.Location = new System.Drawing.Point(6, 31);
+      this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(52, 13);
+      this.label3.Size = new System.Drawing.Size(69, 17);
       this.label3.TabIndex = 18;
       this.label3.Text = "Proxy Url:";
       // 
@@ -164,9 +176,11 @@
       this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
       this.groupBox1.Controls.Add(this._closeConnectionAfterEachRequestCheckBox);
-      this.groupBox1.Location = new System.Drawing.Point(10, 21);
+      this.groupBox1.Location = new System.Drawing.Point(13, 26);
+      this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(514, 80);
+      this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.groupBox1.Size = new System.Drawing.Size(685, 98);
       this.groupBox1.TabIndex = 9;
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "Advanced network settings";
@@ -174,9 +188,10 @@
       // _closeConnectionAfterEachRequestCheckBox
       // 
       this._closeConnectionAfterEachRequestCheckBox.AutoSize = true;
-      this._closeConnectionAfterEachRequestCheckBox.Location = new System.Drawing.Point(13, 19);
+      this._closeConnectionAfterEachRequestCheckBox.Location = new System.Drawing.Point(17, 23);
+      this._closeConnectionAfterEachRequestCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._closeConnectionAfterEachRequestCheckBox.Name = "_closeConnectionAfterEachRequestCheckBox";
-      this._closeConnectionAfterEachRequestCheckBox.Size = new System.Drawing.Size(197, 17);
+      this._closeConnectionAfterEachRequestCheckBox.Size = new System.Drawing.Size(258, 21);
       this._closeConnectionAfterEachRequestCheckBox.TabIndex = 16;
       this._closeConnectionAfterEachRequestCheckBox.Text = "Close connection after each request";
       this._closeConnectionAfterEachRequestCheckBox.UseVisualStyleBackColor = true;
@@ -191,17 +206,20 @@
       this._manualProxyGroupBox.Controls.Add(this.label3);
       this._manualProxyGroupBox.Controls.Add(this._proxyUrlTextBox);
       this._manualProxyGroupBox.Controls.Add(this._userNameTextBox);
-      this._manualProxyGroupBox.Location = new System.Drawing.Point(16, 167);
+      this._manualProxyGroupBox.Location = new System.Drawing.Point(21, 213);
+      this._manualProxyGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._manualProxyGroupBox.Name = "_manualProxyGroupBox";
-      this._manualProxyGroupBox.Size = new System.Drawing.Size(495, 119);
+      this._manualProxyGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this._manualProxyGroupBox.Size = new System.Drawing.Size(660, 139);
       this._manualProxyGroupBox.TabIndex = 10;
       this._manualProxyGroupBox.TabStop = false;
       // 
       // _mappingConfigurationButton
       // 
-      this._mappingConfigurationButton.Location = new System.Drawing.Point(364, 317);
+      this._mappingConfigurationButton.Location = new System.Drawing.Point(485, 401);
+      this._mappingConfigurationButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._mappingConfigurationButton.Name = "_mappingConfigurationButton";
-      this._mappingConfigurationButton.Size = new System.Drawing.Size(158, 23);
+      this._mappingConfigurationButton.Size = new System.Drawing.Size(214, 28);
       this._mappingConfigurationButton.TabIndex = 11;
       this._mappingConfigurationButton.Text = "Mapping Configuration...";
       this._mappingConfigurationButton.UseVisualStyleBackColor = true;
@@ -209,9 +227,9 @@
       // 
       // AdvancedOptionsForm
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(534, 395);
+      this.ClientSize = new System.Drawing.Size(712, 486);
       this.Controls.Add(this._mappingConfigurationButton);
       this.Controls.Add(this._manualProxyGroupBox);
       this.Controls.Add(this.groupBox1);
@@ -219,7 +237,7 @@
       this.Controls.Add(this.buttonCancel);
       this.Controls.Add(this.OkButton);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-      this.Margin = new System.Windows.Forms.Padding(2);
+      this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
       this.Name = "AdvancedOptionsForm";
       this.ShowIcon = false;
       this.Text = "Advanced Options";
