01f5d0d9d1be32173a6e750fa248f38b961189dc
Alexander Nimmervoll <nimm@technikum-wien.at>
2016-02-08 16:21:59 +0100

    Fix button layout and change "Reset Cache" to "Clear Cache".

--- CalDavSynchronizer/Ui/Options/GoogleOptionsDisplayControl.Designer.cs
+++ CalDavSynchronizer/Ui/Options/GoogleOptionsDisplayControl.Designer.cs
@@ -38,9 +38,9 @@ namespace CalDavSynchronizer.Ui.Options
       this._inactiveCheckBox = new System.Windows.Forms.CheckBox();
       this._copyButton = new System.Windows.Forms.Button();
       this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-      this._outlookFolderControl = new OutlookFolderControl();
-      this._serverSettingsControl = new GoogleServerSettingsControl();
-      this._syncSettingsControl = new SyncSettingsControl();
+      this._outlookFolderControl = new CalDavSynchronizer.Ui.Options.OutlookFolderControl();
+      this._serverSettingsControl = new CalDavSynchronizer.Ui.Options.GoogleServerSettingsControl();
+      this._syncSettingsControl = new CalDavSynchronizer.Ui.Options.SyncSettingsControl();
       this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
       this._browseToProfileCacheDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this._resetButton = new System.Windows.Forms.Button();
@@ -50,9 +50,10 @@ namespace CalDavSynchronizer.Ui.Options
       // _advancedOptionsButton
       // 
       this._advancedOptionsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this._advancedOptionsButton.Location = new System.Drawing.Point(269, 518);
+      this._advancedOptionsButton.Location = new System.Drawing.Point(359, 638);
+      this._advancedOptionsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._advancedOptionsButton.Name = "_advancedOptionsButton";
-      this._advancedOptionsButton.Size = new System.Drawing.Size(104, 23);
+      this._advancedOptionsButton.Size = new System.Drawing.Size(139, 28);
       this._advancedOptionsButton.TabIndex = 6;
       this._advancedOptionsButton.Text = "Advanced options";
       this._advancedOptionsButton.UseVisualStyleBackColor = true;
@@ -60,9 +61,10 @@ namespace CalDavSynchronizer.Ui.Options
       // 
       // _deleteButton
       // 
-      this._deleteButton.Location = new System.Drawing.Point(12, 518);
+      this._deleteButton.Location = new System.Drawing.Point(16, 638);
+      this._deleteButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._deleteButton.Name = "_deleteButton";
-      this._deleteButton.Size = new System.Drawing.Size(51, 23);
+      this._deleteButton.Size = new System.Drawing.Size(68, 28);
       this._deleteButton.TabIndex = 4;
       this._deleteButton.Text = "Delete";
       this._deleteButton.UseVisualStyleBackColor = true;
@@ -71,17 +73,19 @@ namespace CalDavSynchronizer.Ui.Options
       // _profileNameTextBox
       // 
       this._profileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this._profileNameTextBox.Location = new System.Drawing.Point(235, 12);
+      this._profileNameTextBox.Location = new System.Drawing.Point(313, 15);
+      this._profileNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._profileNameTextBox.Name = "_profileNameTextBox";
-      this._profileNameTextBox.Size = new System.Drawing.Size(222, 20);
+      this._profileNameTextBox.Size = new System.Drawing.Size(295, 22);
       this._profileNameTextBox.TabIndex = 0;
       // 
       // label10
       // 
       this.label10.AutoSize = true;
-      this.label10.Location = new System.Drawing.Point(20, 15);
+      this.label10.Location = new System.Drawing.Point(27, 18);
+      this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label10.Name = "label10";
-      this.label10.Size = new System.Drawing.Size(68, 13);
+      this.label10.Size = new System.Drawing.Size(91, 17);
       this.label10.TabIndex = 13;
       this.label10.Text = "Profile name:";
       // 
@@ -89,18 +93,20 @@ namespace CalDavSynchronizer.Ui.Options
       // 
       this._inactiveCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
       this._inactiveCheckBox.AutoSize = true;
-      this._inactiveCheckBox.Location = new System.Drawing.Point(375, 522);
+      this._inactiveCheckBox.Location = new System.Drawing.Point(507, 642);
+      this._inactiveCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._inactiveCheckBox.Name = "_inactiveCheckBox";
-      this._inactiveCheckBox.Size = new System.Drawing.Size(78, 17);
+      this._inactiveCheckBox.Size = new System.Drawing.Size(97, 21);
       this._inactiveCheckBox.TabIndex = 7;
       this._inactiveCheckBox.Text = "Deactivate";
       this._inactiveCheckBox.UseVisualStyleBackColor = true;
       // 
       // _copyButton
       // 
-      this._copyButton.Location = new System.Drawing.Point(71, 518);
+      this._copyButton.Location = new System.Drawing.Point(92, 638);
+      this._copyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._copyButton.Name = "_copyButton";
-      this._copyButton.Size = new System.Drawing.Size(44, 23);
+      this._copyButton.Size = new System.Drawing.Size(59, 28);
       this._copyButton.TabIndex = 5;
       this._copyButton.Text = "Copy";
       this._copyButton.UseVisualStyleBackColor = true;
@@ -110,28 +116,30 @@ namespace CalDavSynchronizer.Ui.Options
       // 
       this._outlookFolderControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this._outlookFolderControl.Location = new System.Drawing.Point(12, 38);
+      this._outlookFolderControl.Location = new System.Drawing.Point(16, 47);
+      this._outlookFolderControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._outlookFolderControl.Name = "_outlookFolderControl";
-      this._outlookFolderControl.Size = new System.Drawing.Size(445, 80);
+      this._outlookFolderControl.Size = new System.Drawing.Size(593, 98);
       this._outlookFolderControl.TabIndex = 1;
       // 
       // _serverSettingsControl
       // 
       this._serverSettingsControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this._serverSettingsControl.Location = new System.Drawing.Point(12, 132);
+      this._serverSettingsControl.Location = new System.Drawing.Point(16, 162);
+      this._serverSettingsControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._serverSettingsControl.Name = "_serverSettingsControl";
-      this._serverSettingsControl.Size = new System.Drawing.Size(445, 140);
+      this._serverSettingsControl.Size = new System.Drawing.Size(593, 172);
       this._serverSettingsControl.TabIndex = 2;
       // 
       // _syncSettingsControl
       // 
       this._syncSettingsControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this._syncSettingsControl.Location = new System.Drawing.Point(12, 312);
-      this._syncSettingsControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this._syncSettingsControl.Location = new System.Drawing.Point(16, 384);
+      this._syncSettingsControl.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
       this._syncSettingsControl.Name = "_syncSettingsControl";
-      this._syncSettingsControl.Size = new System.Drawing.Size(445, 200);
+      this._syncSettingsControl.Size = new System.Drawing.Size(593, 246);
       this._syncSettingsControl.TabIndex = 3;
       // 
       // contextMenu
@@ -140,28 +148,29 @@ namespace CalDavSynchronizer.Ui.Options
       this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this._browseToProfileCacheDirectoryToolStripMenuItem});
       this.contextMenu.Name = "contextMenu";
-      this.contextMenu.Size = new System.Drawing.Size(248, 26);
+      this.contextMenu.Size = new System.Drawing.Size(304, 30);
       // 
       // _browseToProfileCacheDirectoryToolStripMenuItem
       // 
       this._browseToProfileCacheDirectoryToolStripMenuItem.Name = "_browseToProfileCacheDirectoryToolStripMenuItem";
-      this._browseToProfileCacheDirectoryToolStripMenuItem.Size = new System.Drawing.Size(247, 22);
+      this._browseToProfileCacheDirectoryToolStripMenuItem.Size = new System.Drawing.Size(303, 26);
       this._browseToProfileCacheDirectoryToolStripMenuItem.Text = "Browse to profile cache directory";
       this._browseToProfileCacheDirectoryToolStripMenuItem.Click += new System.EventHandler(this._browseToProfileCacheDirectoryToolStripMenuItem_Click);
       // 
       // _resetButton
       // 
-      this._resetButton.Location = new System.Drawing.Point(119, 518);
+      this._resetButton.Location = new System.Drawing.Point(159, 638);
+      this._resetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this._resetButton.Name = "_resetButton";
-      this._resetButton.Size = new System.Drawing.Size(78, 23);
+      this._resetButton.Size = new System.Drawing.Size(104, 28);
       this._resetButton.TabIndex = 15;
-      this._resetButton.Text = "Reset Cache";
+      this._resetButton.Text = "Clear Cache";
       this._resetButton.UseVisualStyleBackColor = true;
       this._resetButton.Click += new System.EventHandler(this._resetButton_Click);
       // 
       // GoogleOptionsDisplayControl
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ContextMenuStrip = this.contextMenu;
       this.Controls.Add(this._resetButton);
@@ -174,8 +183,9 @@ namespace CalDavSynchronizer.Ui.Options
       this.Controls.Add(this._deleteButton);
       this.Controls.Add(this._serverSettingsControl);
       this.Controls.Add(this._syncSettingsControl);
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
       this.Name = "GoogleOptionsDisplayControl";
-      this.Size = new System.Drawing.Size(469, 571);
+      this.Size = new System.Drawing.Size(625, 703);
       this.contextMenu.ResumeLayout(false);
       this.ResumeLayout(false);
       this.PerformLayout();
