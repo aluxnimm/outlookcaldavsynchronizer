5c0071a03e462fef629bbaae78d1ee486e0c98ab
Gerhard Zehetbauer <Gerhard.Zehetbauer@chello.at>
2016-02-15 19:41:12 +0100

    Bring data into view if Report contains data.

--- CalDavSynchronizer/Ui/Reports/Views/SynchronizationReportView.xaml.cs
+++ CalDavSynchronizer/Ui/Reports/Views/SynchronizationReportView.xaml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// This file is Part of CalDavSynchronizer (http://outlookcaldavsynchronizer.sourceforge.net/)
+// Copyright (c) 2015 Gerhard Zehetbauer
+// Copyright (c) 2015 Alexander Nimmervoll
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CalDavSynchronizer.Ui.Reports.ViewModels;
+using log4net;
 
 namespace CalDavSynchronizer.Ui.Reports.Views
 {
@@ -20,9 +29,38 @@ namespace CalDavSynchronizer.Ui.Reports.Views
   /// </summary>
   public partial class SynchronizationReportView : UserControl
   {
+    private static readonly ILog s_logger = LogManager.GetLogger (System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType);
+
     public SynchronizationReportView ()
     {
-      InitializeComponent ();
+      InitializeComponent();
+      EntitySynchronizationReportList.DataContextChanged += EntitySynchronizationReportList_DataContextChanged;
+    }
+
+    private void EntitySynchronizationReportList_DataContextChanged (object sender, DependencyPropertyChangedEventArgs e)
+    {
+      try
+      {
+        var reportViewModel = (ReportViewModel) e.NewValue;
+        if (reportViewModel != null)
+        {
+          EntitySynchronizationReportList.SelectedItem = reportViewModel.Report.EntitySynchronizationReports.FirstOrDefault();
+
+          if (reportViewModel.Report.LoadErrors.Length == 0 && reportViewModel.Report.EntitySynchronizationReports.Length > 0)
+            ErrorsTabControl.SelectedItem = EntitySyncReportsTabItem;
+          else
+            ErrorsTabControl.SelectedItem = LoadErrorsTabItem;
+        }
+        else
+        {
+          EntitySynchronizationReportList.SelectedItem = null;
+          ErrorsTabControl.SelectedItem = LoadErrorsTabItem;
+        }
+      }
+      catch (Exception x)
+      {
+        s_logger.Error (null, x);
+      }
     }
   }
 }
