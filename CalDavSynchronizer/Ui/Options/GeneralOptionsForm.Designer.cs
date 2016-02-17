4dce13fef2daebdde636553c74244675d4d4359e
Alexander Nimmervoll <nimm@technikum-wien.at>
2016-02-18 00:33:32 +0100

    Update tooltip for  "Include custom message classes in Outlook filter" general option, since Windows Search Service is recommended but not necessary with the dasl filter fixes.

--- CalDavSynchronizer/Ui/Options/GeneralOptionsForm.Designer.cs
+++ CalDavSynchronizer/Ui/Options/GeneralOptionsForm.Designer.cs
@@ -201,7 +201,8 @@ namespace CalDavSynchronizer.Ui.Options
       this._includeCustomMessageClassesCheckBox.TabIndex = 7;
       this._includeCustomMessageClassesCheckBox.Text = "Include custom message classes in Outlook filter";
       this._toolTip.SetToolTip(this._includeCustomMessageClassesCheckBox, "Use prefix filter to include also custom message_classes in filter for Outlook fo" +
-        "lders. \r\nThis option needs Windows Search Service enabled.\r\n");
+        "lders. \r\nFor better performance, Windows Search Service shouldn\'t be deactivated" +
+        " if this option is enabled.");
       this._includeCustomMessageClassesCheckBox.UseVisualStyleBackColor = true;
       // 
       // groupBox2
