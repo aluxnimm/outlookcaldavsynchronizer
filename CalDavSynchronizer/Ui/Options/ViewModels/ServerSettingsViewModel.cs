2018286b837828e47fbf08dbc877f19938c3b6a7
Gerhard Zehetbauer <Gerhard.Zehetbauer@chello.at>
2016-03-07 19:04:07 +0100

    Use PassworBox for entering password in ServerSettings

--- CalDavSynchronizer/Ui/Options/ViewModels/ServerSettingsViewModel.cs
+++ CalDavSynchronizer/Ui/Options/ViewModels/ServerSettingsViewModel.cs
@@ -1,6 +1,22 @@
-﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿// This file is Part of CalDavSynchronizer (http://outlookcaldavsynchronizer.sourceforge.net/)
+// Copyright (c) 2015 Gerhard Zehetbauer
+// Copyright (c) 2015 Alexander Nimmervoll
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
@@ -11,7 +27,7 @@ namespace CalDavSynchronizer.Ui.Options.ViewModels
 {
   internal class ServerSettingsViewModel : ViewModelBase, IServerSettingsViewModel
   {
-    private static readonly ILog s_logger = LogManager.GetLogger (System.Reflection.MethodBase.GetCurrentMethod ().DeclaringType);
+    private static readonly ILog s_logger = LogManager.GetLogger (MethodBase.GetCurrentMethod().DeclaringType);
 
     private string _calenderUrl;
     private string _emailAddress;
@@ -86,7 +102,7 @@ namespace CalDavSynchronizer.Ui.Options.ViewModels
       }
     }
 
-    public static ServerSettingsViewModel DesignInstance => new ServerSettingsViewModel(NullSettingsFaultFinder.Instance,new DesignCurrentOptions())
+    public static ServerSettingsViewModel DesignInstance => new ServerSettingsViewModel (NullSettingsFaultFinder.Instance, new DesignCurrentOptions())
                                                             {
                                                                 CalenderUrl = "http://calendar.url",
                                                                 EmailAddress = "bla@dot.com",
@@ -94,7 +110,7 @@ namespace CalDavSynchronizer.Ui.Options.ViewModels
                                                                 UseAccountPassword = true,
                                                                 UserName = "username"
                                                             };
-    
+
     public void SetOptions (Contracts.Options options)
     {
       CalenderUrl = options.CalenderUrl;
@@ -128,7 +144,7 @@ namespace CalDavSynchronizer.Ui.Options.ViewModels
 
     private async void TestConnection ()
     {
-      _testConnectionCommand.SetCanExecute(false);
+      _testConnectionCommand.SetCanExecute (false);
       try
       {
         await OptionTasks.TestWebDavConnection (_currentOptions, _settingsFaultFinder);
@@ -145,7 +161,6 @@ namespace CalDavSynchronizer.Ui.Options.ViewModels
       {
         _testConnectionCommand.SetCanExecute (true);
       }
-   
     }
   }
 }
--- CalDavSynchronizer/Ui/Options/Views/ServerSettingsView.xaml.cs
+++ CalDavSynchronizer/Ui/Options/Views/ServerSettingsView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Controls;
+using CalDavSynchronizer.Ui.Options.ViewModels;
 
 namespace CalDavSynchronizer.Ui.Options.Views
 {
@@ -8,9 +9,35 @@ namespace CalDavSynchronizer.Ui.Options.Views
   /// </summary>
   public partial class ServerSettingsView : UserControl
   {
+    private ServerSettingsViewModel _viewModel;
+
     public ServerSettingsView ()
     {
       InitializeComponent();
+      DataContextChanged += ServerSettingsView_DataContextChanged;
+      _passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+    }
+
+    private void PasswordBox_PasswordChanged (object sender, System.Windows.RoutedEventArgs e)
+    {
+      if (_passwordBox.Password != _viewModel.Password)
+        _viewModel.Password = _passwordBox.Password;
+    }
+
+    private void ServerSettingsView_DataContextChanged (object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+    {
+      _viewModel = e.NewValue as ServerSettingsViewModel;
+      if (_viewModel != null)
+      {
+        _passwordBox.Password = _viewModel.Password;
+        _viewModel.PropertyChanged += ViewModel_PropertyChanged;
+      }
+    }
+
+    private void ViewModel_PropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      if (e.PropertyName == nameof (ServerSettingsViewModel.Password))
+        _passwordBox.Password = _viewModel.Password;
     }
   }
 }
