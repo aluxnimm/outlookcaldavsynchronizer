fd353741355059f460de6089a298f0d075098deb
Gerhard Zehetbauer <Gerhard.Zehetbauer@chello.at>
2016-03-15 19:58:25 +0100

    Add components for Google Contacts

---
+++ CalDavSynchronizer/Implementation/GoogleContacts/ContactEntityMapper.cs
@@ -0,0 +1,55 @@
+﻿// This file is Part of CalDavSynchronizer (http://outlookcaldavsynchronizer.sourceforge.net/)
+// Copyright (c) 2015 Gerhard Zehetbauer
+// Copyright (c) 2015 Alexander Nimmervoll
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Reflection;
+using CalDavSynchronizer.Contracts;
+using CalDavSynchronizer.Implementation.ComWrappers;
+using GenSync.EntityMapping;
+using GenSync.Logging;
+using Google.Contacts;
+using log4net;
+
+namespace CalDavSynchronizer.Implementation.GoogleContacts
+{
+  public class ContactEntityMapper : IEntityMapper<ContactItemWrapper, Contact>
+  {
+    private static readonly ILog s_logger = LogManager.GetLogger (MethodInfo.GetCurrentMethod().DeclaringType);
+
+    private readonly ContactMappingConfiguration _configuration;
+
+    public ContactEntityMapper (ContactMappingConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    public Contact Map1To2 (ContactItemWrapper source, Contact target, IEntityMappingLogger logger)
+    {
+      target.Name.GivenName = source.Inner.FirstName;
+      target.Name.FamilyName = source.Inner.LastName;
+      return target;
+    }
+
+    public ContactItemWrapper Map2To1 (Contact source, ContactItemWrapper target, IEntityMappingLogger logger)
+    {
+      target.Inner.FirstName = source.Name.GivenName;
+      target.Inner.LastName = source.Name.FamilyName;
+
+      return target;
+    }
+  }
+}
---
+++ CalDavSynchronizer/Implementation/GoogleContacts/GoogleContactRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GenSync;
+using GenSync.EntityRepositories;
+using GenSync.Logging;
+using Google.Contacts;
+using Google.GData.Client;
+using Google.GData.Contacts;
+
+namespace CalDavSynchronizer.Implementation.GoogleContacts
+{
+  class GoogleContactRepository : IEntityRepository<Contact, string, string>
+  {
+    private readonly ContactsRequest _contactFacade;
+
+    public GoogleContactRepository (ContactsRequest contactFacade)
+    {
+      if (contactFacade == null)
+        throw new ArgumentNullException (nameof (contactFacade));
+
+      _contactFacade = contactFacade;
+    }
+
+    public Task Delete (string entityId, string version)
+    {
+      // calculate url
+      // https://developers.google.com/google-apps/contacts/v3/#retrieving_contacts_using_query_parameters
+      // Contact contact = cr.Retrieve<Contact>("https://www.google.com/m8/feeds/contacts/default/full/contactId");
+
+      Uri feedUri = new Uri (ContactsQuery.CreateContactsUri ("default"));
+      var contactUrl = new Uri (feedUri, entityId);
+      return Task.Run (() => _contactFacade.Delete (contactUrl, version));
+    }
+
+    public Task<EntityVersion<string, string>> Update (string entityId, string version, Contact entityToUpdate, Func<Contact, Contact> entityModifier)
+    {
+      var contact = entityModifier (entityToUpdate);
+      return Task.Run (() =>
+      {
+        var updatedContact = _contactFacade.Update (contact);
+        return EntityVersion.Create (updatedContact.Id, updatedContact.ETag);
+      });
+    }
+
+    public Task<EntityVersion<string, string>> Create (Func<Contact, Contact> entityInitializer)
+    {
+      var contact = entityInitializer (new Contact());
+      Uri feedUri = new Uri (ContactsQuery.CreateContactsUri ("default"));
+      return Task.Run(() =>
+      {
+        var newContact = _contactFacade.Insert (feedUri, contact);
+        return EntityVersion.Create (newContact.Id, newContact.ETag);
+      });
+    }
+
+    public async Task<IReadOnlyList<EntityVersion<string, string>>> GetVersions (IEnumerable<IdWithAwarenessLevel<string>> idsOfEntitiesToQuery)
+    {
+      var idsOfEntitiesToQueryDictionary = idsOfEntitiesToQuery.ToDictionary (i => i.Id);
+
+      return (await GetAllVersions (new string[] { })).Where (v => idsOfEntitiesToQueryDictionary.ContainsKey (v.Id)).ToArray();
+    }
+
+    public Task<IReadOnlyList<EntityVersion<string, string>>> GetAllVersions (IEnumerable<string> idsOfknownEntities)
+    {
+      return Task.Run (() => (IReadOnlyList<EntityVersion<string, string>>) _contactFacade.GetContacts().Entries.Select (c => EntityVersion.Create (c.Id, c.ETag)).ToArray());
+    }
+
+    public Task<IReadOnlyList<EntityWithId<string, Contact>>> Get (ICollection<string> ids, ILoadEntityLogger logger)
+    {
+      return Task.Run (() => (IReadOnlyList<EntityWithId<string, Contact>>) _contactFacade.GetContacts().Entries.Select (c => EntityWithId.Create (c.Id, c)).ToArray());
+    }
+
+    public void Cleanup (IReadOnlyDictionary<string, Contact> entities)
+    {
+
+    }
+  }
+}
