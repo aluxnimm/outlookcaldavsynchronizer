a5177cb55d92248f79db0361bff32bb90e4e97a1
Alexander Nimmervoll <nimm@technikum-wien.at>
2016-11-02 21:20:53 +0100

    Add custom actions to installer to copy registry keys for 64 bit Office installations.

---
+++ CalDavSynchronizer.CustomInstaller/CustomInstaller.Designer.cs
@@ -0,0 +1,36 @@
+﻿namespace CalDavSynchronizer.CustomInstaller
+{
+  partial class CustomInstaller
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Komponenten-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      components = new System.ComponentModel.Container();
+    }
+
+    #endregion
+  }
+}
