906f103510d30719eb80dc90eeb433b7a6879c2e
Gerhard Zehetbauer <Gerhard.Zehetbauer@chello.at>
2016-12-08 00:04:39 +0100

    first version

---
+++ CalDavSynchronizer.Conversions.UnitTests/DocumentConverterFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalDavSynchronizer.Conversions.UnitTests
+{
+  [TestFixture]
+  public class DocumentConverterFixture
+  {
+    [Test]
+    public void ConvertRtfToHtml ()
+    {
+      var html = new DocumentConverter().ConvertRtfToHtml(@"{\rtf1
+ Guten Tag!
+ \line
+ {\i Dies} ist \b{\i ein 
+ \i0 formatierter \b0Text}.
+ \par
+ \i0 Das \b0Ende.
+ }");
+
+    }
+  }
+}
---
+++ CalDavSynchronizer.Conversions/RtfXamlConverter.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace CalDavSynchronizer.Conversions
+{
+  internal class RtfXamlConverter
+  {
+
+    public static string ConvertRtfToXaml (string rtfText)
+    {
+      var flowDocument = new FlowDocument ();
+      if (string.IsNullOrEmpty (rtfText))
+        return "";
+      var textRange = new TextRange (flowDocument.ContentStart, flowDocument.ContentEnd);
+      using (var rtfMemoryStream = new MemoryStream ())
+      {
+        using (var rtfStreamWriter = new StreamWriter (rtfMemoryStream))
+        {
+          rtfStreamWriter.Write (rtfText);
+          rtfStreamWriter.Flush ();
+          rtfMemoryStream.Seek (0, SeekOrigin.Begin);
+          textRange.Load (rtfMemoryStream, DataFormats.Rtf);
+        }
+      }
+      using (var rtfMemoryStream = new MemoryStream ())
+      {
+        textRange = new TextRange (flowDocument.ContentStart, flowDocument.ContentEnd);
+        textRange.Save (rtfMemoryStream, DataFormats.Xaml);
+        rtfMemoryStream.Seek (0, SeekOrigin.Begin);
+        using (var rtfStreamReader = new StreamReader (rtfMemoryStream))
+        {
+          return rtfStreamReader.ReadToEnd ();
+        }
+      }
+    }
+
+    public static string ConvertXamlToRtf (string xamlText)
+    {
+      var flowDocument = new FlowDocument ();
+      if (string.IsNullOrEmpty (xamlText))
+        return "";
+      var textRange = new TextRange (flowDocument.ContentStart, flowDocument.ContentEnd);
+      using (var xamlMemoryStream = new MemoryStream ())
+      {
+        using (var xamlStreamWriter = new StreamWriter (xamlMemoryStream))
+        {
+          xamlStreamWriter.Write (xamlText);
+          xamlStreamWriter.Flush ();
+          xamlMemoryStream.Seek (0, SeekOrigin.Begin);
+          textRange.Load (xamlMemoryStream, DataFormats.Xaml);
+        }
+      }
+      using (var rtfMemoryStream = new MemoryStream ())
+      {
+        textRange = new TextRange (flowDocument.ContentStart, flowDocument.ContentEnd);
+        textRange.Save (rtfMemoryStream, DataFormats.Rtf);
+        rtfMemoryStream.Seek (0, SeekOrigin.Begin);
+        using (var rtfStreamReader = new StreamReader (rtfMemoryStream))
+        {
+          return rtfStreamReader.ReadToEnd ();
+        }
+      }
+    }
+  }
+}
