4a0b77dca2687f8e0633ce6029aec325f5082a99
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2017-06-17 22:12:45 +0200

    Revert "Remove parallelism since it takes approx 40% longer that sequential approach."
    This reverts commit 3c51a16198b01b04eeb3e59133e81d59be846a6a.
    Reason: Measured in Debug Build instead of Release

--- CalDavSynchronizer/Implementation/CalDavRepository.cs
+++ CalDavSynchronizer/Implementation/CalDavRepository.cs
@@ -49,7 +49,7 @@ namespace CalDavSynchronizer.Implementation
 
   public class CalDavRepository<TContext> : IEntityRepository<WebResourceName, string, IICalendar, TContext>
   {
-    private static readonly ILog s_logger = LogManager.GetLogger(MethodInfo.GetCurrentMethod().DeclaringType);
+    private static readonly ILog s_logger = LogManager.GetLogger (MethodInfo.GetCurrentMethod ().DeclaringType);
 
     private readonly ICalDavDataAccess _calDavDataAccess;
     private readonly IStringSerializer _calendarSerializer;
@@ -57,7 +57,7 @@ namespace CalDavSynchronizer.Implementation
     private readonly IDateTimeRangeProvider _dateTimeRangeProvider;
     private readonly bool _deleteAndCreateOnUpdateError403;
 
-    public CalDavRepository(
+    public CalDavRepository (
       ICalDavDataAccess calDavDataAccess,
       IStringSerializer calendarSerializer,
       CalDavRepository.EntityType entityType,
@@ -71,43 +71,43 @@ namespace CalDavSynchronizer.Implementation
       _dateTimeRangeProvider = dateTimeRangeProvider;
     }
 
-    public async Task<IEnumerable<EntityVersion<WebResourceName, string>>> GetVersions(IEnumerable<IdWithAwarenessLevel<WebResourceName>> idsOfEntitiesToQuery, TContext context)
+    public async Task<IEnumerable<EntityVersion<WebResourceName, string>>> GetVersions (IEnumerable<IdWithAwarenessLevel<WebResourceName>> idsOfEntitiesToQuery, TContext context)
     {
-      return await _calDavDataAccess.GetVersions(idsOfEntitiesToQuery.Select(i => i.Id));
+      return await _calDavDataAccess.GetVersions (idsOfEntitiesToQuery.Select (i => i.Id));
     }
 
-    public async Task<IEnumerable<EntityVersion<WebResourceName, string>>> GetAllVersions(IEnumerable<WebResourceName> idsOfknownEntities, TContext context)
+    public async Task<IEnumerable<EntityVersion<WebResourceName, string>>> GetAllVersions (IEnumerable<WebResourceName> idsOfknownEntities, TContext context)
     {
-      using (AutomaticStopwatch.StartInfo(s_logger, "CalDavRepository.GetVersions"))
+      using (AutomaticStopwatch.StartInfo (s_logger, "CalDavRepository.GetVersions"))
       {
         switch (_entityType)
         {
           case CalDavRepository.EntityType.Event:
 
-            return await _calDavDataAccess.GetEventVersions(_dateTimeRangeProvider.GetRange());
+            return await _calDavDataAccess.GetEventVersions (_dateTimeRangeProvider.GetRange ());
           case CalDavRepository.EntityType.Todo:
-            return await _calDavDataAccess.GetTodoVersions(_dateTimeRangeProvider.GetRange());
+            return await _calDavDataAccess.GetTodoVersions (_dateTimeRangeProvider.GetRange ());
           default:
-            throw new NotImplementedException(string.Format("EntityType '{0}' not implemented.", _entityType));
+            throw new NotImplementedException (string.Format ("EntityType '{0}' not implemented.", _entityType));
         }
       }
     }
 
-    public async Task<IEnumerable<EntityWithId<WebResourceName, IICalendar>>> Get(ICollection<WebResourceName> ids, ILoadEntityLogger logger, TContext context)
+    public async Task<IEnumerable<EntityWithId<WebResourceName, IICalendar>>> Get (ICollection<WebResourceName> ids, ILoadEntityLogger logger, TContext context)
     {
       if (ids.Count == 0)
         return new EntityWithId<WebResourceName, IICalendar>[] { };
 
-      using (AutomaticStopwatch.StartInfo(s_logger, string.Format("CalDavRepository.Get ({0} entitie(s))", ids.Count)))
+      using (AutomaticStopwatch.StartInfo (s_logger, string.Format ("CalDavRepository.Get ({0} entitie(s))", ids.Count)))
       {
-        var entities = await _calDavDataAccess.GetEntities(ids);
-        return await ParallelDeserialize(entities, logger);
+        var entities = await _calDavDataAccess.GetEntities (ids);
+        return await ParallelDeserialize (entities, logger);
       }
     }
     
-    public Task VerifyUnknownEntities(Dictionary<WebResourceName, string> unknownEntites, TContext context)
+    public Task VerifyUnknownEntities (Dictionary<WebResourceName, string> unknownEntites, TContext context)
     {
-      return Task.FromResult(0);
+      return Task.FromResult (0);
     }
 
     public void Cleanup(IICalendar entity)
@@ -120,64 +120,77 @@ namespace CalDavSynchronizer.Implementation
       // nothing to do
     }
 
-    private Task<IReadOnlyList<EntityWithId<WebResourceName, IICalendar>>> ParallelDeserialize(IReadOnlyList<EntityWithId<WebResourceName, string>> serializedEntities, ILoadEntityLogger logger)
+    private Task<IReadOnlyList<EntityWithId<WebResourceName, IICalendar>>> ParallelDeserialize (IReadOnlyList<EntityWithId<WebResourceName, string>> serializedEntities, ILoadEntityLogger logger)
     {
-      return Task.Factory.StartNew(() =>
+      return Task.Factory.StartNew (() =>
       {
-        var result = new List<EntityWithId<WebResourceName, IICalendar>>();
+        var result = new List<EntityWithId<WebResourceName, IICalendar>> ();
 
-        foreach (var serialized in serializedEntities)
-        {
-          IICalendar calendar;
-          string normalizedICalData, fixedICalData;
-
-          // fix some linebreak issues with Open-Xchange
-          if (serialized.Entity.Contains("\r\r\n"))
-          {
-            normalizedICalData = CalendarDataPreprocessor.NormalizeLineBreaks(serialized.Entity);
-          }
-          else
-          {
-            normalizedICalData = serialized.Entity;
-          }
-
-          // emClient sets DTSTART in VTIMEZONE to year 0001, which causes a 90 sec delay in DDay.iCal to evaluate the recurrence rule.
-          // If we find such a DTSTART we replace it 0001 with 1970 since the historic data is not valid anyway and avoid the performance issue.
-          if (normalizedICalData.Contains("DTSTART:00010101"))
-          {
-            fixedICalData = CalendarDataPreprocessor.FixInvalidDTSTARTInTimeZoneNoThrow(normalizedICalData);
-            s_logger.InfoFormat("Changed DTSTART from year 0001 to 1970 in VTIMEZONE of ICalData '{0}'.", serialized.Id);
-          }
-          else
-          {
-            fixedICalData = normalizedICalData;
-          }
-
-          if (TryDeserializeCalendar(fixedICalData, out calendar, serialized.Id, _calendarSerializer, NullLoadEntityLogger.Instance))
-          {
-            // Add only if there is atleast one vevent or vtodo to avoid Nullreference Exceptions when processing
-            if ((_entityType == CalDavRepository.EntityType.Event && calendar.Events.Count > 0) ||
-                (_entityType == CalDavRepository.EntityType.Todo && calendar.Todos.Count > 0))
+        Parallel.ForEach (
+            serializedEntities,
+            () => Tuple.Create (new iCalendarSerializer (), new List<Tuple<WebResourceName, IICalendar>> ()),
+            (serialized, loopState, threadLocal) =>
             {
-              result.Add(EntityWithId.Create(serialized.Id, calendar));
-            }
-          }
-          else
-          {
-            // maybe deserialization failed because of the iCal-TimeZone-Bug =>  try to fix it
-            var fixedICalData2 = CalendarDataPreprocessor.FixTimeZoneComponentOrderNoThrow(fixedICalData);
-            if (TryDeserializeCalendar(fixedICalData2, out calendar, serialized.Id, _calendarSerializer, logger))
-            {
-              // Add only if there is atleast one vevent or vtodo to avoid Nullreference Exceptions when processing
-              if ((_entityType == CalDavRepository.EntityType.Event && calendar.Events.Count > 0) ||
-                  (_entityType == CalDavRepository.EntityType.Todo && calendar.Todos.Count > 0))
+              IICalendar calendar;
+              string normalizedICalData, fixedICalData;
+
+              // fix some linebreak issues with Open-Xchange
+              if (serialized.Entity.Contains ("\r\r\n"))
               {
-                result.Add(EntityWithId.Create(serialized.Id, calendar));
-                s_logger.Info(string.Format("Deserialized ICalData with reordering of TimeZone data '{0}'.", serialized.Id));
+                normalizedICalData = CalendarDataPreprocessor.NormalizeLineBreaks (serialized.Entity);
               }
-            }
-          }
-        }
+              else
+              {
+                normalizedICalData = serialized.Entity;
+              }
+
+              // emClient sets DTSTART in VTIMEZONE to year 0001, which causes a 90 sec delay in DDay.iCal to evaluate the recurrence rule.
+              // If we find such a DTSTART we replace it 0001 with 1970 since the historic data is not valid anyway and avoid the performance issue.
+              if (normalizedICalData.Contains ("DTSTART:00010101"))
+              {
+                fixedICalData = CalendarDataPreprocessor.FixInvalidDTSTARTInTimeZoneNoThrow (normalizedICalData);
+                s_logger.InfoFormat ("Changed DTSTART from year 0001 to 1970 in VTIMEZONE of ICalData '{0}'.", serialized.Id);
+              }
+              else
+              {
+                fixedICalData = normalizedICalData;
+              }
+
+              if (TryDeserializeCalendar (fixedICalData, out calendar, serialized.Id, threadLocal.Item1, NullLoadEntityLogger.Instance))
+              {
+                // Add only if there is atleast one vevent or vtodo to avoid Nullreference Exceptions when processing
+                if ((_entityType == CalDavRepository.EntityType.Event && calendar.Events.Count > 0) ||
+                    (_entityType == CalDavRepository.EntityType.Todo && calendar.Todos.Count > 0))
+                {
+                  threadLocal.Item2.Add (Tuple.Create (serialized.Id, calendar));
+                }
+              }
+              else
+              {
+                // maybe deserialization failed because of the iCal-TimeZone-Bug =>  try to fix it
+                var fixedICalData2 = CalendarDataPreprocessor.FixTimeZoneComponentOrderNoThrow (fixedICalData);
+                if (TryDeserializeCalendar (fixedICalData2, out calendar, serialized.Id, threadLocal.Item1, logger))
+                {
+                  // Add only if there is atleast one vevent or vtodo to avoid Nullreference Exceptions when processing
+                  if ((_entityType == CalDavRepository.EntityType.Event && calendar.Events.Count > 0) ||
+                      (_entityType == CalDavRepository.EntityType.Todo && calendar.Todos.Count > 0))
+                  {
+                    threadLocal.Item2.Add (Tuple.Create (serialized.Id, calendar));
+                    s_logger.Info (string.Format ("Deserialized ICalData with reordering of TimeZone data '{0}'.", serialized.Id));
+                  }
+                }
+              }
+
+              return threadLocal;
+            },
+            threadLocal =>
+            {
+              lock (result)
+              {
+                foreach (var calendar in threadLocal.Item2)
+                  result.Add (EntityWithId.Create (calendar.Item1, calendar.Item2));
+              }
+            });
 
        IReadOnlyList<EntityWithId<WebResourceName, IICalendar>> readOnlyResult = result;
         return readOnlyResult;
@@ -185,43 +198,43 @@ namespace CalDavSynchronizer.Implementation
     }
 
 
-    public async Task<bool> TryDelete(WebResourceName entityId, string version, TContext context)
+    public async Task<bool> TryDelete (WebResourceName entityId, string version, TContext context)
     {
-      using (AutomaticStopwatch.StartDebug(s_logger))
+      using (AutomaticStopwatch.StartDebug (s_logger))
       {
-        return await _calDavDataAccess.TryDeleteEntity(entityId, version);
+        return await _calDavDataAccess.TryDeleteEntity (entityId, version);
       }
     }
 
-    public async Task<EntityVersion<WebResourceName, string>> TryUpdate(
-      WebResourceName entityId,
-      string entityVersion,
-      IICalendar entityToUpdate,
-      Func<IICalendar, Task<IICalendar>> entityModifier,
-      TContext context)
+    public async Task<EntityVersion<WebResourceName, string>> TryUpdate (
+        WebResourceName entityId,
+        string entityVersion,
+        IICalendar entityToUpdate,
+        Func<IICalendar, Task<IICalendar>> entityModifier,
+        TContext context)
     {
-      using (AutomaticStopwatch.StartDebug(s_logger))
+      using (AutomaticStopwatch.StartDebug (s_logger))
       {
-        var updatedEntity = await entityModifier(entityToUpdate);
+        var updatedEntity = await entityModifier (entityToUpdate);
         try
         {
-          return await _calDavDataAccess.TryUpdateEntity(entityId, entityVersion, SerializeCalendar(updatedEntity));
+          return await _calDavDataAccess.TryUpdateEntity (entityId, entityVersion, SerializeCalendar (updatedEntity));
         }
         catch (WebDavClientException ex)
         {
           if (_deleteAndCreateOnUpdateError403 && ex.StatusCode == System.Net.HttpStatusCode.Forbidden)
           {
-            s_logger.Warn("Server returned '403' ('Forbidden') for update, trying Delete and Recreate instead...");
+            s_logger.Warn ("Server returned '403' ('Forbidden') for update, trying Delete and Recreate instead...");
 
-            await TryDelete(entityId, entityVersion, context);
+            await TryDelete (entityId, entityVersion, context);
 
-            var uid = Guid.NewGuid().ToString();
+            var uid = Guid.NewGuid ().ToString ();
             if (updatedEntity.Events.Count > 0)
               updatedEntity.Events[0].UID = uid;
             else
               updatedEntity.Todos[0].UID = uid;
 
-            return await _calDavDataAccess.CreateEntity(SerializeCalendar(updatedEntity), uid);
+            return await _calDavDataAccess.CreateEntity (SerializeCalendar (updatedEntity), uid);
           }
           else
           {
@@ -231,24 +244,24 @@ namespace CalDavSynchronizer.Implementation
       }
     }
 
-    public async Task<EntityVersion<WebResourceName, string>> Create(Func<IICalendar, Task<IICalendar>> entityInitializer, TContext context)
+    public async Task<EntityVersion<WebResourceName, string>> Create (Func<IICalendar, Task<IICalendar>> entityInitializer, TContext context)
     {
-      using (AutomaticStopwatch.StartDebug(s_logger))
+      using (AutomaticStopwatch.StartDebug (s_logger))
       {
-        IICalendar newCalendar = new iCalendar();
-        newCalendar = await entityInitializer(newCalendar);
+        IICalendar newCalendar = new iCalendar ();
+        newCalendar = await entityInitializer (newCalendar);
         var uid = (newCalendar.Events.Count > 0) ? newCalendar.Events[0].UID : newCalendar.Todos[0].UID;
-        return await _calDavDataAccess.CreateEntity(SerializeCalendar(newCalendar), uid);
+        return await _calDavDataAccess.CreateEntity (SerializeCalendar (newCalendar), uid);
       }
     }
 
 
-    private string SerializeCalendar(IICalendar calendar)
+    private string SerializeCalendar (IICalendar calendar)
     {
-      return _calendarSerializer.SerializeToString(calendar);
+      return _calendarSerializer.SerializeToString (calendar);
     }
 
-    private static bool TryDeserializeCalendar(
+    private static bool TryDeserializeCalendar (
       string iCalData,
       out IICalendar calendar,
       WebResourceName uriOfCalendarForLogging,
@@ -258,23 +271,23 @@ namespace CalDavSynchronizer.Implementation
       calendar = null;
       try
       {
-        calendar = DeserializeCalendar(iCalData, calendarSerializer);
+        calendar = DeserializeCalendar (iCalData, calendarSerializer);
         return true;
       }
       catch (Exception x)
       {
-        s_logger.Error(string.Format("Could not deserialize ICalData of '{0}'.", uriOfCalendarForLogging.OriginalAbsolutePath));
-        s_logger.Debug(string.Format("ICalData:\r\n{0}", iCalData), x);
-        logger.LogSkipLoadBecauseOfError(uriOfCalendarForLogging, x);
+        s_logger.Error (string.Format ("Could not deserialize ICalData of '{0}'.", uriOfCalendarForLogging.OriginalAbsolutePath));
+        s_logger.Debug (string.Format ("ICalData:\r\n{0}", iCalData), x);
+        logger.LogSkipLoadBecauseOfError (uriOfCalendarForLogging, x);
         return false;
       }
     }
 
-    private static IICalendar DeserializeCalendar(string iCalData, IStringSerializer calendarSerializer)
+    private static IICalendar DeserializeCalendar (string iCalData, IStringSerializer calendarSerializer)
     {
-      using (var reader = new StringReader(iCalData))
+      using (var reader = new StringReader (iCalData))
       {
-        var calendarCollection = (iCalendarCollection)calendarSerializer.Deserialize(reader);
+        var calendarCollection = (iCalendarCollection) calendarSerializer.Deserialize (reader);
         return calendarCollection[0];
       }
     }
