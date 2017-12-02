77056088002837928f7d29fc90a4fde89bb9c7e6
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2017-12-02 13:30:39 +0100

    Add string resource files

--- CalDavSynchronizer/Globalization/Strings.cs
+++ CalDavSynchronizer/Globalization/Strings.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿//#define RECORD_RESOURCES
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -16,7 +20,35 @@ namespace CalDavSynchronizer.Globalization
 
     public static string Localize(string text)
     {
-      return Regex.Replace(text,"[A-Za-z]","_");
+#if RECORD_RESOURCES
+      AddResource(text);
+      return Regex.Replace(text, "[A-Za-z]", "_");
+#else
+      var translation = StringResources.ResourceManager.GetString(text);
+      return translation ?? text;
+#endif
     }
+
+
+#if RECORD_RESOURCES
+    private static void AddResource(string text)
+    {
+      const string resxFileName = @"D:\dev\CalDavSynchronizer\CalDavSynchronizer\Globalization\StringResources.de-DE.resx";
+      var allResources = new ResXResourceSet(resxFileName);
+      if (allResources.GetString(text) == null)
+      {
+        using (var writer = new ResXResourceWriter(resxFileName))
+        {
+          foreach (DictionaryEntry existingEntry in allResources)
+          {
+            writer.AddResource((string)existingEntry.Key, existingEntry.Value);
+          }
+          writer.AddResource(text, text);
+        }
+      }
+    }
+#endif
+
+
   }
 }
