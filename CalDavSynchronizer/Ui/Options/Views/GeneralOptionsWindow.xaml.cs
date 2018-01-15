6e23cbc849c874a623932e73699914624cdfcfb3
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2018-01-15 18:28:19 +0100

    Add View

---
+++ CalDavSynchronizer/Ui/Options/Views/GeneralOptionsWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿// This file is Part of CalDavSynchronizer (http://outlookcaldavsynchronizer.sourceforge.net/)
+// Copyright (c) 2015 Gerhard Zehetbauer
+// Copyright (c) 2015 Alexander Nimmervoll
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Windows;
+using CalDavSynchronizer.Ui.Options.ViewModels;
+
+namespace CalDavSynchronizer.Ui.Options.Views
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class GeneralOptionsWindow : Window
+  {
+    public GeneralOptionsWindow()
+    {
+      InitializeComponent ();
+      this.DataContextChanged += OptionsWindow_DataContextChanged;
+    }
+
+    private void OptionsWindow_DataContextChanged (object sender, DependencyPropertyChangedEventArgs e)
+    {
+      var viewModel = e.NewValue as OptionsCollectionViewModel;
+      if (viewModel != null)
+      {
+        viewModel.CloseRequested += ViewModel_CloseRequested;
+      }
+    }
+
+    private void ViewModel_CloseRequested (object sender, CloseEventArgs e)
+    {
+      DialogResult = e.IsAcceptedByUser;
+    }
+  }
+}
