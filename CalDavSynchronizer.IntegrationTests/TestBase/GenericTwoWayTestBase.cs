9daaa21034fed453f711dfb931361c655c779a37
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2018-09-04 23:58:20 +0200

    Reformat code

--- CalDavSynchronizer.IntegrationTests/TestBase/GenericTwoWayTestBase.cs
+++ CalDavSynchronizer.IntegrationTests/TestBase/GenericTwoWayTestBase.cs
@@ -24,191 +24,189 @@ using NUnit.Framework;
 
 namespace CalDavSynchronizer.IntegrationTests.TestBase
 {
-  [TestFixture]
-  public abstract class GenericTwoWayTestBase<TAId,TBId, TSynchronizer> 
-    where TSynchronizer : TestSynchronizerBase
-  {
-    protected abstract IEqualityComparer<TAId> AIdComparer { get; }
-    protected abstract IEqualityComparer<TBId> BIdComparer { get; }
-    
-    protected abstract IReadOnlyList<(TAId AId, TBId BId)> GetRelations();
-
-    protected abstract Task<IReadOnlyList<TAId>> CreateInA(IEnumerable<string> contents);
-    protected abstract Task<IReadOnlyList<TBId>> CreateInB(IEnumerable<string> contents);
-
-    protected abstract Task UpdateInA(TAId id, string content);
-    protected abstract Task UpdateInB(TBId id, string content);
-
-    protected abstract Task<string> GetFromA(TAId id);
-    protected abstract Task<string> GetFromB(TBId id);
-
-    protected abstract Task DeleteInA(TAId id);
-    protected abstract Task DeleteInB(TBId id);
-
-    protected TSynchronizer Synchronizer { get; private set; }
-    protected abstract Options GetOptions();
-    protected abstract TSynchronizer CreateSynchronizer(Options options);
-
-    protected async Task InitializeSynchronizer(int? chunkSize, bool useWebDavCollectionSync)
+    [TestFixture]
+    public abstract class GenericTwoWayTestBase<TAId, TBId, TSynchronizer>
+        where TSynchronizer : TestSynchronizerBase
     {
-      var options = GetOptions();
-      options.ChunkSize = chunkSize ?? 0;
-      options.IsChunkedSynchronizationEnabled = chunkSize.HasValue;
-      options.UseWebDavCollectionSync = useWebDavCollectionSync;
-      Synchronizer = CreateSynchronizer(options);
-      await Synchronizer.Initialize();
+        protected abstract IEqualityComparer<TAId> AIdComparer { get; }
+        protected abstract IEqualityComparer<TBId> BIdComparer { get; }
+
+        protected abstract IReadOnlyList<(TAId AId, TBId BId)> GetRelations();
+
+        protected abstract Task<IReadOnlyList<TAId>> CreateInA(IEnumerable<string> contents);
+        protected abstract Task<IReadOnlyList<TBId>> CreateInB(IEnumerable<string> contents);
+
+        protected abstract Task UpdateInA(TAId id, string content);
+        protected abstract Task UpdateInB(TBId id, string content);
+
+        protected abstract Task<string> GetFromA(TAId id);
+        protected abstract Task<string> GetFromB(TBId id);
+
+        protected abstract Task DeleteInA(TAId id);
+        protected abstract Task DeleteInB(TBId id);
+
+        protected TSynchronizer Synchronizer { get; private set; }
+        protected abstract Options GetOptions();
+        protected abstract TSynchronizer CreateSynchronizer(Options options);
+
+        protected async Task InitializeSynchronizer(int? chunkSize, bool useWebDavCollectionSync)
+        {
+            var options = GetOptions();
+            options.ChunkSize = chunkSize ?? 0;
+            options.IsChunkedSynchronizationEnabled = chunkSize.HasValue;
+            options.UseWebDavCollectionSync = useWebDavCollectionSync;
+            Synchronizer = CreateSynchronizer(options);
+            await Synchronizer.Initialize();
+        }
+
+
+
+        public virtual async Task Test(int? chunkSize, int itemsPerOperation, bool useWebDavCollectionSync)
+        {
+            await InitializeSynchronizer(chunkSize, useWebDavCollectionSync);
+
+            // Set maximum alloed open items to chunksize+1, since the synchronizer builds chunks with operations that load an entity and keep it open
+            // A delete operation in a Outlook repository will open an entity and release it immediately
+            var maximumOpenItemsPerType = chunkSize + 1;
+
+
+            Synchronizer.ClearCache();
+            await Synchronizer.ClearEventRepositoriesAndCache();
+
+            Assert.That(
+                (await CreateInA(Enumerable.Range(0, itemsPerOperation * 3).Select(i => $"Item {i}"))).Count,
+                Is.EqualTo(itemsPerOperation * 3));
+
+            Assert.That(
+                (await CreateInB(Enumerable.Range(0, itemsPerOperation * 3).Select(i => $"Item {i}"))).Count,
+                Is.EqualTo(itemsPerOperation * 3));
+
+            int nextName = itemsPerOperation * 3;
+
+            var newAids = await CreateInA(Enumerable.Range(0, itemsPerOperation).Select(i => $"Item {nextName++}"));
+            var newBids = await CreateInB(Enumerable.Range(0, itemsPerOperation).Select(i => $"Item {nextName++}"));
+
+            Assert.That(newAids.Count, Is.EqualTo(itemsPerOperation));
+            Assert.That(newBids.Count, Is.EqualTo(itemsPerOperation));
+
+            await Synchronizer.SynchronizeAndCheck(
+                unchangedA: itemsPerOperation * 3, addedA: itemsPerOperation, changedA: 0, deletedA: 0,
+                unchangedB: itemsPerOperation * 3, addedB: itemsPerOperation, changedB: 0, deletedB: 0,
+                createA: itemsPerOperation, updateA: 0, deleteA: 0,
+                createB: itemsPerOperation, updateB: 0, deleteB: 0,
+                ordinalOfReportToCheck: OrdinalOfReportToCheck,
+                maximumOpenItemsPerType: maximumOpenItemsPerType);
+
+            var relations = GetRelations();
+            Assert.That(relations.Count, Is.EqualTo(itemsPerOperation * 5));
+
+            var updatedA = new HashSet<TAId>(AIdComparer);
+            var updatedB = new HashSet<TBId>(BIdComparer);
+            var notUpdated = new HashSet<(TAId AId, TBId BId)>(new RelationEqualityComparer(AIdComparer, BIdComparer));
+            var deletedA = new HashSet<TAId>(AIdComparer);
+            var deletedB = new HashSet<TBId>(BIdComparer);
+
+            for (var i = 0; i < relations.Count; i++)
+            {
+                var relation = relations[i];
+
+                if (i % 5 == 0)
+                {
+                    await UpdateInA(relation.AId, "Upd" + await GetFromA(relation.AId));
+                    updatedA.Add(relation.AId);
+                }
+                else if (i % 5 == 1)
+                {
+                    await UpdateInB(relation.BId, "Upd" + await GetFromB(relation.BId));
+                    updatedB.Add(relation.BId);
+                }
+                else if (i % 5 == 2)
+                {
+                    await DeleteInA(relation.AId);
+                    deletedA.Add(relation.AId);
+                }
+                else if (i % 5 == 3)
+                {
+                    await DeleteInB(relation.BId);
+                    deletedB.Add(relation.BId);
+                }
+                else
+                {
+                    notUpdated.Add(relation);
+                }
+            }
+
+            Assert.That(updatedA.Count, Is.EqualTo(itemsPerOperation));
+            Assert.That(updatedB.Count, Is.EqualTo(itemsPerOperation));
+            Assert.That(deletedA.Count, Is.EqualTo(itemsPerOperation));
+            Assert.That(deletedB.Count, Is.EqualTo(itemsPerOperation));
+            Assert.That(notUpdated.Count, Is.EqualTo(itemsPerOperation));
+
+            await Synchronizer.SynchronizeAndCheck(
+                unchangedA: itemsPerOperation * 3, addedA: 0, changedA: itemsPerOperation, deletedA: itemsPerOperation,
+                unchangedB: itemsPerOperation * 3, addedB: 0, changedB: itemsPerOperation, deletedB: itemsPerOperation,
+                createA: 0, updateA: itemsPerOperation, deleteA: itemsPerOperation,
+                createB: 0, updateB: itemsPerOperation, deleteB: itemsPerOperation,
+                ordinalOfReportToCheck: OrdinalOfReportToCheck,
+                maximumOpenItemsPerType: maximumOpenItemsPerType);
+
+            var relations2 = GetRelations();
+
+            Assert.That(relations2.Count, Is.EqualTo(itemsPerOperation * 3));
+
+            foreach (var relation in relations2)
+            {
+                Assert.That(deletedA.Contains(relation.AId), Is.False);
+                Assert.That(deletedB.Contains(relation.BId), Is.False);
+
+                var aContent = await GetFromA(relation.AId);
+                var bContent = await GetFromB(relation.BId);
+
+
+                if (updatedA.Remove(relation.AId))
+                {
+                    Assert.That(aContent, Does.StartWith("Upd"));
+                    Assert.That(bContent, Does.StartWith("Upd"));
+                }
+                else if (updatedB.Remove(relation.BId))
+                {
+                    Assert.That(aContent, Does.StartWith("Upd"));
+                    Assert.That(bContent, Does.StartWith("Upd"));
+                }
+                else if (notUpdated.Remove(relation))
+                {
+                    Assert.That(aContent, Does.Not.StartWith("Upd"));
+                    Assert.That(bContent, Does.Not.StartWith("Upd"));
+                }
+            }
+
+            Assert.That(updatedA.Count, Is.EqualTo(0));
+            Assert.That(updatedB.Count, Is.EqualTo(0));
+            Assert.That(notUpdated.Count, Is.EqualTo(0));
+        }
+
+        protected virtual int? OrdinalOfReportToCheck => null;
+
+        class RelationEqualityComparer : IEqualityComparer<(TAId AId, TBId BId)>
+        {
+            private readonly IEqualityComparer<TAId> _aIdComparer;
+            private readonly IEqualityComparer<TBId> _bIdComparer;
+
+            public RelationEqualityComparer(IEqualityComparer<TAId> aIdComparer, IEqualityComparer<TBId> bIdComparer)
+            {
+                _aIdComparer = aIdComparer ?? throw new ArgumentNullException(nameof(aIdComparer));
+                _bIdComparer = bIdComparer ?? throw new ArgumentNullException(nameof(bIdComparer));
+            }
+
+            public bool Equals((TAId AId, TBId BId) x, (TAId AId, TBId BId) y)
+            {
+                return _aIdComparer.Equals(x.AId, y.AId) && _bIdComparer.Equals(x.BId, y.BId);
+            }
+
+            public int GetHashCode((TAId AId, TBId BId) obj)
+            {
+                return _aIdComparer.GetHashCode(obj.AId) * 397 ^ _bIdComparer.GetHashCode(obj.BId);
+            }
+        }
     }
-
-
-  
-    public virtual async Task Test(int? chunkSize, int itemsPerOperation, bool useWebDavCollectionSync)
-    {
-      await InitializeSynchronizer(chunkSize, useWebDavCollectionSync);
-
-      // Set maximum alloed open items to chunksize+1, since the synchronizer builds chunks with operations that load an entity and keep it open
-      // A delete operation in a Outlook repository will open an entity and release it immediately
-      var maximumOpenItemsPerType = chunkSize +1;
-
-
-      Synchronizer.ClearCache();
-      await Synchronizer.ClearEventRepositoriesAndCache();
-
-      Assert.That(
-        (await CreateInA(Enumerable.Range(0, itemsPerOperation * 3).Select(i => $"Item {i}"))).Count,
-        Is.EqualTo(itemsPerOperation * 3));
-
-      Assert.That(
-        (await CreateInB(Enumerable.Range(0, itemsPerOperation * 3).Select(i => $"Item {i}"))).Count,
-        Is.EqualTo(itemsPerOperation * 3));
-
-      int nextName = itemsPerOperation*3;
-
-      var newAids = await CreateInA(Enumerable.Range(0, itemsPerOperation).Select(i => $"Item {nextName++}"));
-      var newBids = await CreateInB(Enumerable.Range(0, itemsPerOperation).Select(i => $"Item {nextName++}"));
-
-      Assert.That(newAids.Count, Is.EqualTo(itemsPerOperation));
-      Assert.That(newBids.Count, Is.EqualTo(itemsPerOperation));
-      
-      await Synchronizer.SynchronizeAndCheck(
-        unchangedA: itemsPerOperation * 3, addedA: itemsPerOperation, changedA: 0, deletedA: 0,
-        unchangedB: itemsPerOperation * 3, addedB: itemsPerOperation, changedB: 0, deletedB: 0,
-        createA: itemsPerOperation, updateA: 0, deleteA: 0,
-        createB: itemsPerOperation, updateB: 0, deleteB: 0,
-        ordinalOfReportToCheck:  OrdinalOfReportToCheck,
-        maximumOpenItemsPerType: maximumOpenItemsPerType);
-      
-      var relations = GetRelations();
-      Assert.That(relations.Count, Is.EqualTo(itemsPerOperation * 5));
-
-      var updatedA = new HashSet<TAId>(AIdComparer);
-      var updatedB = new HashSet<TBId>(BIdComparer);
-      var notUpdated = new HashSet<(TAId AId, TBId BId)>(new RelationEqualityComparer(AIdComparer, BIdComparer));
-      var deletedA = new HashSet<TAId>(AIdComparer);
-      var deletedB = new HashSet<TBId>(BIdComparer);
-
-      for (var i=0; i < relations.Count; i++)
-      {
-        var relation = relations[i];
-
-        if (i % 5 == 0)
-        {
-          await UpdateInA(relation.AId, "Upd" + await GetFromA(relation.AId));
-          updatedA.Add(relation.AId);
-        }
-        else if (i % 5 == 1)
-        {
-          await UpdateInB(relation.BId, "Upd" + await GetFromB(relation.BId));
-          updatedB.Add(relation.BId);
-        }
-        else if (i % 5 == 2)
-        {
-          await DeleteInA(relation.AId);
-          deletedA.Add(relation.AId);
-        }
-        else if (i % 5 == 3)
-        {
-          await DeleteInB(relation.BId);
-          deletedB.Add(relation.BId);
-        }
-        else
-        {
-          notUpdated.Add(relation);
-        }
-      }
-
-      Assert.That(updatedA.Count, Is.EqualTo(itemsPerOperation));
-      Assert.That(updatedB.Count, Is.EqualTo(itemsPerOperation));
-      Assert.That(deletedA.Count, Is.EqualTo(itemsPerOperation));
-      Assert.That(deletedB.Count, Is.EqualTo(itemsPerOperation));
-      Assert.That(notUpdated.Count, Is.EqualTo(itemsPerOperation));
-
-      await Synchronizer.SynchronizeAndCheck(
-        unchangedA: itemsPerOperation*3, addedA: 0, changedA: itemsPerOperation, deletedA: itemsPerOperation,
-        unchangedB: itemsPerOperation*3, addedB: 0, changedB: itemsPerOperation, deletedB: itemsPerOperation,
-        createA: 0, updateA: itemsPerOperation, deleteA: itemsPerOperation,
-        createB: 0, updateB: itemsPerOperation, deleteB: itemsPerOperation,
-        ordinalOfReportToCheck: OrdinalOfReportToCheck,
-        maximumOpenItemsPerType: maximumOpenItemsPerType);
-
-      var relations2 = GetRelations();
-
-      Assert.That(relations2.Count, Is.EqualTo(itemsPerOperation * 3));
-
-      foreach (var relation in relations2)
-      {
-        Assert.That(deletedA.Contains(relation.AId), Is.False);
-        Assert.That(deletedB.Contains(relation.BId), Is.False);
-
-        var aContent = await GetFromA(relation.AId);
-        var bContent = await GetFromB(relation.BId);
-
-
-        if (updatedA.Remove(relation.AId))
-        {
-          Assert.That(aContent, Does.StartWith("Upd"));
-          Assert.That(bContent, Does.StartWith("Upd"));
-        }
-        else if (updatedB.Remove(relation.BId))
-        {
-          Assert.That(aContent, Does.StartWith("Upd"));
-          Assert.That(bContent, Does.StartWith("Upd"));
-        }
-        else if (notUpdated.Remove(relation))
-        {
-          Assert.That(aContent, Does.Not.StartWith("Upd"));
-          Assert.That(bContent, Does.Not.StartWith("Upd"));
-        }
-      }
-
-      Assert.That(updatedA.Count, Is.EqualTo(0));
-      Assert.That(updatedB.Count, Is.EqualTo(0));
-      Assert.That(notUpdated.Count, Is.EqualTo(0));
-    }
-
-    protected virtual int? OrdinalOfReportToCheck => null;
-
-    class RelationEqualityComparer : IEqualityComparer<(TAId AId, TBId BId)>
-    {
-      private readonly IEqualityComparer<TAId> _aIdComparer;
-      private readonly IEqualityComparer<TBId> _bIdComparer;
-
-      public RelationEqualityComparer(IEqualityComparer<TAId> aIdComparer, IEqualityComparer<TBId> bIdComparer)
-      {
-        _aIdComparer = aIdComparer ?? throw new ArgumentNullException(nameof(aIdComparer));
-        _bIdComparer = bIdComparer ?? throw new ArgumentNullException(nameof(bIdComparer));
-      }
-
-      public bool Equals((TAId AId, TBId BId) x, (TAId AId, TBId BId) y)
-      {
-        return _aIdComparer.Equals(x.AId, y.AId) && _bIdComparer.Equals(x.BId, y.BId);
-      }
-
-      public int GetHashCode((TAId AId, TBId BId) obj)
-      {
-        return _aIdComparer.GetHashCode(obj.AId) * 397 ^ _bIdComparer.GetHashCode(obj.BId);
-      }
-    }
-  }
-
-
 }
