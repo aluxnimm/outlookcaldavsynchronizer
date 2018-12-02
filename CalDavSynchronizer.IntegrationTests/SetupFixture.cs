341fd3202c6706618d3cd28bf08ff60e5d7a318c
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2018-12-02 15:32:16 +0100

    Improve tests

---
+++ CalDavSynchronizer.IntegrationTests/SetupFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Config;
+using NUnit.Framework;
+
+namespace CalDavSynchronizer.IntegrationTests
+{
+    [SetUpFixture]
+    public class SetupFixture
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
