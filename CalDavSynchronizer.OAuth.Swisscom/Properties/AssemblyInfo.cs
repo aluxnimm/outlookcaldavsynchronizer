aa2a9a0a5dfac983dce39053c859a013d63388a1
Alexander Nimmervoll <nimm@caldavsynchronizer.org>
2020-06-29 17:25:43 +0200

    Fix assembly info version for OAuth.Swisscom project and link to CalDavSynchronizer files.

--- CalDavSynchronizer.OAuth.Swisscom/Properties/AssemblyInfo.cs
+++ CalDavSynchronizer.OAuth.Swisscom/Properties/AssemblyInfo.cs
@@ -7,14 +7,6 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CalDavSynchronizer.OAuth.Swisscom")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CalDavSynchronizer.OAuth.Swisscom")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -51,5 +43,3 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
