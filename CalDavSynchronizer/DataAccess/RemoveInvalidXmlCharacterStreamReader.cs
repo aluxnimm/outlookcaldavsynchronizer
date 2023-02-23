faa062b5a29b36b06149a3decd2cf532e173cc3b
nimm <nimm@technikum-wien.at>
2023-02-23 19:52:24 +0100

    Improve performance of read. Use unicode replacement character, allow xml surrogate pairs.

--- CalDavSynchronizer/DataAccess/RemoveInvalidXmlCharacterStreamReader.cs
+++ CalDavSynchronizer/DataAccess/RemoveInvalidXmlCharacterStreamReader.cs
@@ -21,50 +21,56 @@ using System.Xml;
 
 public class RemoveInvalidXmlCharacterStreamReader : StreamReader
 {
+    // used to save last char from read buffer if it is part of a surrogate pair
+    private int _lastBufferChar = -1;
+    private readonly char _replacementCharacter = '\uFFFD';
     public RemoveInvalidXmlCharacterStreamReader(Stream stream, Encoding encoding) : base(stream, encoding)
     {
     }
-
-    public override int Peek()
-    {
-        var ch = base.Peek();
-        if (ch != -1 && IsInvalidChar(ch))
-        {
-            return Peek();
-        }
-        return ch;
-    }
-
-    public override int Read()
-    {
-        var ch = base.Read();
-        if (ch != -1 && IsInvalidChar(ch))
-        {
-            return Read();
-        }
-        return ch;
-    }
-
     public override int Read(char[] buffer, int index, int count)
     {
-        int readCount = 0, ch;
+        int charsRead = base.Read(buffer, index, count);
+        int bufferEnd = index + charsRead;
 
-        for (int i = 0; i < count && (ch = Read()) != -1; i++)
+        for (int i = index; i < bufferEnd; i++)
         {
-            readCount++;
-            buffer[index + i] = (char)ch;
+            // check if current char is valid xml
+            if (!XmlConvert.IsXmlChar(buffer[i]))
+            {
+                // special case check if there is a surrogate pair with first char of this buffer and last char of old buffer
+                if (index == i && _lastBufferChar != -1)
+                {
+                    if (!XmlConvert.IsXmlSurrogatePair(buffer[i],(char)_lastBufferChar))
+                    {
+                        buffer[i] = _replacementCharacter;
+                    }
+                    _lastBufferChar = -1;
+                }
+                // special case check if there is a surrogate pair with last char of this buffer and first char of next buffer
+                else if (bufferEnd == i + 1)
+                {
+                    int nextBufferFirstChar = Peek();
+                    if (XmlConvert.IsXmlSurrogatePair(nextBufferFirstChar != -1 ? (char)nextBufferFirstChar : '\0', buffer[i]))
+                    {
+                        _lastBufferChar = buffer[i];
+                    }
+                    else
+                    {
+                        buffer[i] = _replacementCharacter;
+                    }
+                }
+                // skip next character validation if it is a surrogate pair
+                else if (XmlConvert.IsXmlSurrogatePair(buffer[i + 1], buffer[i]))
+                {
+                    ++i;
+                }
+                else
+                {
+                    // If the current character is not a valid XML character and not inside a surrogate pair, replace it with a the unicode replacement character.
+                    buffer[i] = _replacementCharacter;
+                }
+            }
         }
-
-        return readCount;
-    }
-
-    private static bool IsInvalidChar(int ch)
-    {
-        return IsInvalidChar((char)ch);
-    }
-
-    private static bool IsInvalidChar(char ch)
-    {
-        return !XmlConvert.IsXmlChar(ch);
+        return charsRead;
     }
 }
