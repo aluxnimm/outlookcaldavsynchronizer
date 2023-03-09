f91cfb4aab99ef4120402fdd44011a08278cd8b2
nimm <nimm@technikum-wien.at>
2023-03-09 14:28:24 +0100

    Fix typo in pkce request.

--- CalDavSynchronizer.OAuth.Swisscom/PKCEAuthorizationCodeRequestUrl.cs
+++ CalDavSynchronizer.OAuth.Swisscom/PKCEAuthorizationCodeRequestUrl.cs
@@ -6,7 +6,7 @@ namespace CalDavSynchronizer.OAuth.Swisscom
     public class PKCEAuthorizationCodeRequestUrl : AuthorizationCodeRequestUrl
     {
        
-        [Google.Apis.Util.RequestParameterAttribute("code_challange", Google.Apis.Util.RequestParameterType.Query)]
+        [Google.Apis.Util.RequestParameterAttribute("code_challenge", Google.Apis.Util.RequestParameterType.Query)]
         public string CodeChallenge { get; set; }
 
         [Google.Apis.Util.RequestParameterAttribute("code_challenge_method", Google.Apis.Util.RequestParameterType.Query)]
